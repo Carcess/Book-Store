6bad607926e1b551861d2f2d6102b391b236ce5b
Carcess <99830414+Carcess@users.noreply.github.com>
2023-03-07 16:34:19 +0500

    Rendering books from JSON

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,134 +1,83 @@
-//Variables
-const courses = document.querySelector('#courses-list'),
-      shoppingCartContent = document.querySelector('#cart-content tbody'),
-      clearCartBtn        = document.querySelector("#clear-cart");
-var coursesList         = [];
-
-//Listener
-loadEventListeners();
-//Load course from localStorage if course avaible
-getCoursesFromLocalStorage();
-
-function loadEventListeners(){
-  courses.addEventListener('click',buyCourse);
-  shoppingCartContent.addEventListener('click',removeCourse);
-  clearCartBtn.addEventListener('click',clearCart);
-}
 
 
 
-//function
 
-function buyCourse(e){
-  e.preventDefault();
-
-   if (e.target.classList.contains("add-to-cart")){
-       const course = e.target.parentElement.parentElement;
-       getCourseInfo(course);
-   }
-}
-
-//get Course Information
-function getCourseInfo(course){
-   const courseInfo = {
-     image : course.querySelector('img').src,
-     title : course.querySelector('h4').textContent,
-     price : course.querySelector('.price span').textContent,
-     id    : course.querySelector('a').getAttribute('data-id')
-   }
-   
-
-   addToCart(courseInfo);
-   coursesList.push(courseInfo);
-   addCourseToLocalStorage();
-   console.log(coursesList);
-}
-
-function addToCart(courseInfo){
-
-  const row = document.createElement('tr');
-  //create an image element
-  image     = document.createElement('img');
-  image.src = courseInfo.image;
-  image.style.width = '100px';
-  //append element to row (parent)
-  row.appendChild(addToTd(image,1));
-  row.appendChild(addToTd(courseInfo.title,0));
-  row.appendChild(addToTd(courseInfo.price,0));
-  //create anchor for delete button
-  const anchor = document.createElement('a');
-  anchor.href = '#';
-  anchor.setAttribute('data-id',courseInfo.id);
-  anchor.textContent = 'X';
-  anchor.classList = 'remove';
-
-  row.appendChild(addToTd(anchor,1));
-  //append to Shooping case
-  shoppingCartContent.appendChild(row);
-}
-
-
-
-function addToTd(info,i){
-    const  td = document.createElement('td');
-  if (i == 0){
-    td.textContent = info
-  }else{
-    td.appendChild(info);
-  }
-  return td
-}
-
-
-function updateShoppingCart(){
-  coursesList.forEach(function(course){
-     addToCart(course);
-  })
-}
-
-
-function removeCourse(e){
-  if (e.target.classList.contains('remove')){
-      let id = e.target.getAttribute('data-id');
-      deleteFromLocalStorage(id);
-    e.target.parentElement.parentElement.remove();
-  }
-
-}
-
-
-
-function clearCart(e){
-  e.preventDefault();
-  while(shoppingCartContent.firstChild){
-    shoppingCartContent.removeChild(shoppingCartContent.firstChild);
-  }
-  coursesList = [];
-  addCourseToLocalStorage();
-
-}
-
-
-function addCourseToLocalStorage(){
-  localStorage.setItem("courses",JSON.stringify(coursesList))
-}
-
-function getCoursesFromLocalStorage(){
-  const cs = localStorage.getItem('courses');
-
-  if (cs === null){
-      coursesList = [];
-  }else{
-    coursesList = JSON.parse(cs);
-    updateShoppingCart();
-  }
-}
-
-function deleteFromLocalStorage(id){
-  coursesList.forEach(function(course,i){
-      if (course.id === id){
-        coursesList.splice(i,1)
+filterSelection("UX")
+function filterSelection(c) {
+  fetch('assets/books.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("HTTP error " + response.status);
       }
-  })
-  addCourseToLocalStorage();
+      return response.json();
+    })
+    .then(json => {
+
+      var FilteredBooks = json.filter(books => books.category === c)
+      // console.log(FilteredBooks);
+      var booksDiv = document.querySelector('#booksRend')
+      // console.log(booksDiv)
+      var BooksToShow = FilteredBooks.map((obj) => {
+
+        return `<div class="container">
+      <div class="d-flex justify-content-center row">
+          <div class="col-md-10">
+              <div class="row p-2 bg-white border rounded mt-2">
+                  <div class="col-md-3 mt-1">
+                  <img class="img-fluid img-responsive rounded product-image" src=${obj.image}></div>
+                  <div class="col-md-6 mt-1">
+                      <h5>${obj.title}</h5>
+                      <p>${obj.author}</p>
+                      <img src="assets/img/stars.png">
+                      <p class="text-justify text-truncate para mb-0">${obj.description}</p>
+                  </div>
+                  <div class="align-items-center align-content-center col-md-3 border-left mt-1">
+                      <div class="d-flex flex-row align-items-center justify-content-evenly">
+                          <h3 class="mr-1">$${obj.price}</h3><span class="strike-text h1">$${obj.price + 10}</span>
+                      </div>
+                      <h6 class="text-success">Free shipping</h6>
+                      <div class="d-flex flex-column mt-4">
+                      <button class="btn button-secondary " type="button" onclick="JavaScript:window.location.hash = '#1'">Details</button>
+                      <button class="btn button-primary mt-2" type="button" data-id=${obj.id}>Add to Cart</button></div>
+                  </div>
+              </div>
+              
+          </div>
+      </div>
+  </div>`
+      })
+
+      booksDiv.innerHTML = BooksToShow.join("")
+
+    })
+    .catch(function () {
+      this.dataError = true;
+    })
+
+
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
 }
